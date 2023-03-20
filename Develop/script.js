058fd8a167e7c9f14d5023d59296c1ec4cf70424
XyrillaSC <professionalstevenkramer@gmail.com>
2023-03-20 13:49:51 -0400

    Added all cases of gen

--- Develop/script.js
+++ Develop/script.js
@@ -19,44 +19,30 @@ let specChar = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`]
 // request params
 
 
-function requestParams() {
 
-  let numOfChar = prompt(`How many characters would you like, friend? (Between 8 - 128 preferably)`)
+let numOfChar = prompt(`How many characters would you like, friend? (Between 8 - 128 preferably)`)
 
-  if (numOfChar < 8 || numOfChar > 128) {
-    alert(`No can do, you read the rules, try again`)
-    return
-  }
-
-  let inclUpper = confirm(`Now, could I interest you in Uppercase Letters?`)
-  let inclLower = confirm(`How about Lowercase Letters?`)
-  let inclNum = confirm(`Are you a fan of Numbers in your passwords?`)
-  let inclSpec = confirm(`And how about those Special Characters?`)
-
-  if (!inclUpper && !inclLower && !inclNum && !inclSpec) {
-    alert(`Well, what do you want then?`)
-  }
-
-  console.log(numOfChar)
-  console.log(inclUpper)
-  console.log(inclLower)
-  console.log(inclNum)
-  console.log(inclSpec)
-
-
-
-
-
-
-
+if (numOfChar < 8 || numOfChar > 128) {
+  alert(`No can do, you read the rules, try again`)
 }
-requestParams()
+
+let inclUpper = confirm(`Now, could I interest you in Uppercase Letters?`)
+let inclLower = confirm(`How about Lowercase Letters?`)
+let inclNum = confirm(`Are you a fan of Numbers in your passwords?`)
+let inclSpec = confirm(`And how about those Special Characters?`)
+
+if (!inclUpper && !inclLower && !inclNum && !inclSpec) {
+  alert(`Well, what do you want then?`)
+}
 
 console.log(numOfChar)
 console.log(inclUpper)
 console.log(inclLower)
 console.log(inclNum)
 console.log(inclSpec)
+
+
+
 
 
 // for each requested char, a random compatible array is chosen, and a random char from that array is selected, and then added to the password.
@@ -81,26 +67,123 @@ function genSpecChar() {
   return randomSpecChar
 }
 
+function getCase1() {
+  let funcArray = [genUpperChar(), genLowerChar(), genNumChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase2() {
+  let funcArray = [genUpperChar(), genLowerChar(), genNumChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase3() {
+  let funcArray = [genUpperChar(), genLowerChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase4() {
+  let funcArray = [genUpperChar(), genNumChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase5() {
+  let funcArray = [genLowerChar(), genNumChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase6() {
+  let funcArray = [genUpperChar(), genLowerChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase7() {
+  let funcArray = [genUpperChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase8() {
+  let funcArray = [genNumChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase9() {
+  let funcArray = [genLowerChar(), genNumChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase10() {
+  let funcArray = [genLowerChar(), genSpecChar()]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+function getCase11() {
+  let funcArray = [genUpperChar(), genNumChar(),]
+  let i = Math.floor(Math.random() * funcArray.length)
+  return funcArray[i]
+}
+
+var buildPassword = ``
 
 
+if (inclUpper && inclLower && inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase1()}`
+}
+else if (inclUpper && inclLower && inclNum && !inclSpec) {
+  var buildPassword = buildPassword + `${getCase2()}`
+}
+if (inclUpper && inclLower && !inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase3()}`
+}
+if (inclUpper && !inclLower && inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase4()}`
+}
+if (!inclUpper && inclLower && inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase5()}`
+}
+if (inclUpper && inclLower && !inclNum && !inclSpec) {
+  var buildPassword = buildPassword + `${getCase6()}`
+}
+if (inclUpper && !inclLower && !inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase7()}`
+}
+if (!inclUpper && !inclLower && inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase8()}`
+}
+if (!inclUpper && inclLower && inclNum && !inclSpec) {
+  var buildPassword = buildPassword + `${getCase9()}`
+}
+if (!inclUpper && inclLower && !inclNum && inclSpec) {
+  var buildPassword = buildPassword + `${getCase10()}`
+}
+if (inclUpper && !inclLower && inclNum && !inclSpec) {
+  var buildPassword = buildPassword + `${getCase11()}`
+}
 if (inclUpper && !inclLower, !inclNum, !inclSpec) {
-  let buildPassword = buildPassword + `${genUpperChar()}`
+  var buildPassword = buildPassword + `${genUpperChar()}`
 }
-
 if (inclLower && !inclUpper, !inclNum, !inclSpec) {
- let buildPassword = buildPassword + `${genLowerChar()}`
+  var buildPassword = buildPassword + `${genLowerChar()}`
 }
-
 if (inclNum && !inclUpper, !inclLower, !inclSpec) {
-
+  var buildPassword = buildPassword + `${genNumChar()}`
 }
-
 if (inclSpec && !inclUpper, !inclLower, !inclNum) {
-
+  var buildPassword = buildPassword + `${genSpecChar()}`
 }
 
+console.log(buildPassword)
 
-let buildPassword = ``
 
 
 // Add event listener to generate button
