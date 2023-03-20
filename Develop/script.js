8c912817cfec0f9e7c0adb3fa04eb42f29402f53
XyrillaSC <professionalstevenkramer@gmail.com>
2023-03-20 11:07:52 -0400

    add charsets

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,13 @@ function writePassword() {
 
 }
 
+// define charset arrays
+let lowerChar = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`]
+let upperChar = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`]
+let numChar = [`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`0`]
+let specChar = [`!`,`@`,`#`,`$`,`%`,`^`,`&`,`*`]
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
